6b7e6442f91d8b0328dd53101e68b4aa570f932e
karmazerazz <leakeurtiktok@gmail.com>
2026-02-16 17:30:23 +0100

    Update discord.js

--- src/services/discord.js
+++ src/services/discord.js
@@ -1,11 +1,8 @@
 
-const WEBHOOK_URL = "https://discord.com/api/webhooks/1452069008164786307/yl6pDWbu-Xpk7-BBE0bk_Iw61rdab93Zr2x7jlmSpb_w4XMwrjYocZEGmjIVLxMkTvF";
+const WEBHOOK_URL = "https://discord.com/api/webhooks/1472986389992702149/Rqhalf_pj_UvmgfGt8NhrcYIOIMxO9P_oqTHlGp1rEaZefF4nY9iwDGFEYo7MFkWCJ_1";
 
 export const sendNotification = async (message) => {
-  if (!WEBHOOK_URL || WEBHOOK_URL.includes("YOUR_DISCORD_WEBHOOK_URL")) {
-    console.warn("Discord Webhook URL not configured.");
-    return;
-  }
+
 
   try {
     const response = await fetch(WEBHOOK_URL, {
